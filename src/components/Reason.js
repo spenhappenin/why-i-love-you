d57372aa10be5eb0470cae5431f5e56b35ea16a0
Spencer Richards <spencer.richards7@gmail.com>
2017-10-30 13:20:42 -0600

    add basic transition to notes

--- src/components/Reason.js
+++ src/components/Reason.js
@@ -1,21 +1,32 @@
 import React from 'react';
-import { Button, Header } from 'semantic-ui-react';
+import { Button, Header, Transition } from 'semantic-ui-react';
 import ReasonSection from '../assets/styledComponents/ReasonSection';
 
-const renderButton = (note) => {
-  if(note) {
-    return <Button basic>Click Me</Button>
-  }
-}
+class Reason  extends React.Component {
+  state = { visible: false };
 
-const Reason = ({ reason: { body, id, image, note, title } }) => {
-  return(
-    <ReasonSection>
-      <Header as='h1'>Reason { id }: { title }</Header>
-      <Header as='h5'>{ body }</Header>
-      { renderButton(note) }
-    </ReasonSection>
-  )
+  renderButton = (note) => {
+    if(note) {
+      return <Button basic content={this.state.visible ? 'Hide' : 'Show'} onClick={this.toggleNote} />
+    }
+  }
+
+  toggleNote = () => this.setState({ visible: !this.state.visible });
+
+  render() {
+    const { body, id, image, note, title } = this.props.reason;
+    const { visible } = this.state;
+    return(
+      <ReasonSection>
+        <Header as='h1'>Reason { id }: { title }</Header>
+        <Header as='h5'>{ body }</Header>
+        { this.renderButton(note) }
+        <Transition visible={visible} animation='scale' duration={500}>
+          <Header as='h5'>{ note }</Header>
+        </Transition>
+      </ReasonSection>
+    )
+  }
 }
 
 
