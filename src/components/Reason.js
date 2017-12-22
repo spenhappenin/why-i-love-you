f5196c369ac647a5526c037b3b93e610f94432f5
Spencer Richards <spencer.richards7@gmail.com>
2017-12-22 11:55:13 -0700

    image button and transition added

--- src/components/Reason.js
+++ src/components/Reason.js
@@ -1,41 +1,62 @@
 import React from 'react';
-import { Button, Header, Segment, Transition } from 'semantic-ui-react';
-import StyledButton from '../assets/styledComponents/StyledButton';
-import ReasonTitle from '../assets/styledComponents/ReasonTitle';
 import ReasonBody from '../assets/styledComponents/ReasonBody';
 import ReasonSection from '../assets/styledComponents/ReasonSection';
+import ReasonTitle from '../assets/styledComponents/ReasonTitle';
+import StyledButton from '../assets/styledComponents/StyledButton';
+import { Button, Header, Image, Segment, Transition } from 'semantic-ui-react';
 
 class Reason  extends React.Component {
-  state = { visible: false };
+  state = { imageVisible: false, noteVisible: false };
 
-  renderButton = (note) => {
+  renderNoteButton = (note) => {
     if(note) {
-      return (
+      return(
         <Button 
           as={StyledButton}
           circular 
           icon='mail' 
           size='huge'
-          color={this.state.visible ? 'brown' : 'red'} 
+          color={this.state.noteVisible ? 'brown' : 'red'} 
           onClick={this.toggleNote} 
         />
       )
     }
   }
 
-  toggleNote = () => this.setState({ visible: !this.state.visible });
+  renderImageButton = (image) => {
+    if(image) {
+      return(
+        <Button
+          as={StyledButton}
+          circular
+          icon='picture'
+          size='huge'
+          color={this.state.imageVisible ? 'brown' : 'red'}
+          onClick={this.toggleImage}
+        />
+      )
+    }
+  }
+
+  toggleNote = () => this.setState({ noteVisible: !this.state.noteVisible });
+
+  toggleImage = () => this.setState({ imageVisible: !this.state.imageVisible });
 
   render() {
-    const { body, id, note, title } = this.props.reason;
-    const { visible } = this.state;
+    const { body, id, image, note, title } = this.props.reason;
+    const { noteVisible, imageVisible } = this.state;
     return(
       <ReasonSection>
         <Header as={ReasonTitle} textAlign='center'>Reason { id }: { title }</Header>
         <Header as={ReasonBody} textAlign='center'>{ body }</Header>
         <Segment basic textAlign='center'>
-          { this.renderButton(note) }
+          { this.renderNoteButton(note) }
+          { this.renderImageButton(image) }
         </Segment>
-        <Transition visible={visible} animation='scale' duration={500}>
+        <Transition visible={imageVisible} animation='scale' duration={500} unmountOnHide={true}>
+          <Image src={image} />
+        </Transition>
+        <Transition visible={noteVisible} animation='scale' duration={500}>
           <Header as='h4' textAlign='center'>{ note }</Header>
         </Transition>
       </ReasonSection>
--- src/data/reasons.js
+++ src/data/reasons.js
@@ -1,43 +1,46 @@
+import Placeholder from '../assets/images/mainheader.JPG';
+
 export const testReasons = [
   {
     id: 1,
     title: 'This is a title...',
     body: 'This is the text that will describe the reason why. This is the text that will describe the reason why. This is the text that will describe the reason why.',
-    image: 'This will be an image at some point'
   },
   {
     id: 2,
     title: 'This is a title...',
     body: 'This is the text that will describe the reason why. This is the text that will describe the reason why. This is the text that will describe the reason why.',
     note: 'This can be a cute little note or something smaller.',
-    image: 'This will be an image at some point'
   },
   {
     id: 3,
     title: 'This is a title...',
     body: 'This is the text that will describe the reason why. This is the text that will describe the reason why. This is the text that will describe the reason why.',
-    image: 'This will be an image at some point'
+    image: Placeholder
   },
   {
     id: 4,
     title: 'This is a title...',
     body: 'This is the text that will describe the reason why. This is the text that will describe the reason why. This is the text that will describe the reason why.',
     note: 'This can be a cute little note or something smaller.',
-    image: 'This will be an image at some point'
   },
   {
     id: 5,
     title: 'This is a title...',
     body: 'This is the text that will describe the reason why. This is the text that will describe the reason why. This is the text that will describe the reason why.',
     note: 'This can be a cute little note or something smaller.',
-    image: 'This will be an image at some point'
   },
   {
     id: 6,
     title: 'This is a title...',
     body: 'This is the text that will describe the reason why. This is the text that will describe the reason why. This is the text that will describe the reason why.',
-    image: 'This will be an image at some point'
-  },        
+  },   
+  {
+    id: 7,
+    title: 'This is a title...',
+    body: 'This is the text that will describe the reason why. This is the text that will describe the reason why. This is the text that will describe the reason why.',
+    image: Placeholder
+  },      
 ]
 
 export const reasons = [
